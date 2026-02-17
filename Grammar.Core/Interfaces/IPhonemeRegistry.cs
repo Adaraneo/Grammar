97f974e5c6827cca0328ada1a805a2618add3da4
Adaraneo <fiftypisoftware@gmail.com>
2026-02-17 16:06:12 +0100

    Added PhonemeRegistry intervace to Grammar.Core

---
+++ Grammar.Core/Interfaces/IPhonemeRegistry.cs
@@ -0,0 +1,21 @@
+﻿using Grammar.Core.Models.Phonology;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grammar.Core.Interfaces
+{
+    public interface IPhonemeRegistry
+    {
+        Phoneme? Get(string symbol);
+        Phoneme? Get(char symbol) => Get(symbol.ToString());
+
+        bool IsVowel(char c);
+        bool IsConsonant(char c);
+        bool IsFrontVowel(char c);
+
+        IReadOnlyCollection<Phoneme> AllPhonemes { get; }
+    }
+}
